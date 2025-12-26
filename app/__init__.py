800cd534945af874eedb4f6da60577adb2be39b2
rsinghackwest <rsingh@ackwest.com>
2025-12-27 00:37:02 +0530

    v5.5.97

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ from flask_limiter.util import get_remote_address
 import os
 import logging
 
-__version__ = "5.5.96"
+__version__ = "5.5.97"
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
