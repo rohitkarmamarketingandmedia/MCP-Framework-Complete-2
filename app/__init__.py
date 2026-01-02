c7df6516a28faebb2ce8ca173d74a47f7493b5be
rsinghackwest <rsingh@ackwest.com>
2026-01-03 01:07:38 +0530

    v5.5.114

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 import os
 import logging
 
-__version__ = "5.5.113"
+__version__ = "5.5.114"
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
